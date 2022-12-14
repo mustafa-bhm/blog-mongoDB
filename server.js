b12fd1f6cf03f53076fe8f98007122a348575ce4
mustafa <mustafa.bhm3@gmail.com>
2022-12-14 15:06:39 -0700

    adding routes for dif paths

--- server.js
+++ server.js
@@ -4,21 +4,24 @@ const fs = require("fs");
 // to create a  server
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method);
-  // **** to send text content ***** //
-  // //set headre content type
-  // res.setHeader("content-type", "text/html");
 
-  // // set the content of resonse
-  // res.write("<h1>hello, world</h1>");
-  // res.write("<h2>hello, again</h2>");
-
-  // // ending the response to send it to te browser
-  // res.end();
+  let path = "./views/";
+  switch (req.url) {
+    case "/":
+      path += "index.html";
+      break;
+    case "/about":
+      path += "about.html";
+      break;
+    default:
+      path += "404.html";
+      break;
+  }
 
   /// *** to send html file as a response **** ///
   res.setHeader("content-type", "text/html");
   // to send html file
-  fs.readFile("./views/index.html", (err, data) => {
+  fs.readFile(path, (err, data) => {
     if (err) {
       console.log(err);
     } else {
