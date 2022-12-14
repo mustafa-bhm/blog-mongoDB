f8bcef19562f7922845a3795356afa25dd57ac4f
mustafa <mustafa.bhm3@gmail.com>
2022-12-14 15:16:09 -0700

    redirecting

--- server.js
+++ server.js
@@ -9,12 +9,20 @@ const server = http.createServer((req, res) => {
   switch (req.url) {
     case "/":
       path += "index.html";
+      res.statusCode = 200;
       break;
     case "/about":
       path += "about.html";
+      res.statusCode = 200;
+      break;
+    case "/about-me":
+      res.statusCode = 301;
+      res.setHeader("Location", "/about");
+      res.end();
       break;
     default:
       path += "404.html";
+      res.statusCode = 404;
       break;
   }
 
@@ -27,6 +35,7 @@ const server = http.createServer((req, res) => {
     } else {
       // res.write(data);
       // or
+
       res.end(data);
     }
   });
