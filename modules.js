a109f1fe82e853e643aac1c2bf0558de915d66df
mustafa <mustafa.bhm3@gmail.com>
2022-12-14 10:48:59 -0700

    reading, writing and deleting files in node

--- modules.js
+++ modules.js
@@ -1,1 +1,8 @@
-const xyz = require("./people");
+const { people, ages } = require("./people");
+
+console.log(people);
+console.log(ages);
+
+const os = require("os");
+
+console.log(os.platform(), os.homedir());
--- people.js
+++ people.js
@@ -1,2 +1,5 @@
 const people = ["Mike", "Alain", "Tom"];
-console.log(people);
+const ages = [20, 25, 30];
+// console.log(people);
+
+module.exports = { people, ages };
