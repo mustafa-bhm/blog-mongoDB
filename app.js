c5a34211b8683eaae891d948750ab683cbe16aee
mustafa <mustafa.bhm3@gmail.com>
2022-12-15 10:02:41 -0700

    adding ejs view engine

--- app.js
+++ app.js
@@ -1,23 +1,25 @@
 const express = require("express");
 const app = express();
 
+//register view engine
+app.set("view engine", "ejs");
+
 /// listening for requests
 app.listen(3000);
 
+// homepage
 app.get("/", (req, res) => {
-  // res.send("index");
-  res.sendFile("./views/index.html", { root: __dirname });
+  res.render("index", { title: "Home" });
 });
 app.get("/about", (req, res) => {
-  res.sendFile("./views/about.html", { root: __dirname });
+  res.render("about", { title: "About" });
 });
 
-// redirecting
-app.get("/about-us", (req, res) => {
-  res.redirect("/about");
+app.get("/blogs/create", (req, res) => {
+  res.render("create", { title: "Create new blog" });
 });
 
 /// 404 page
 app.use((req, res) => {
-  res.status(404).sendFile("./views/404.html", { root: __dirname });
+  res.status(404).render("404", { title: "404" });
 });
