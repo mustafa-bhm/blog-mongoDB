51f6efbc82ba5acebb56677ce35b521acbb39a93
mustafa <mustafa.bhm3@gmail.com>
2022-12-16 12:05:24 -0700

    get details of a single blog

--- app.js
+++ app.js
@@ -56,6 +56,16 @@ app.post("/blogs", (req, res) => {
     });
 });
 
+app.get("/blogs/:id", (req, res) => {
+  const id = req.params.id;
+  Blog.findById(id)
+    .then((result) => {
+      res.render("details", { blog: result, title: "Blog details" });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 app.get("/blogs/create", (req, res) => {
   res.render("create", { title: "Create new blog" });
 });
